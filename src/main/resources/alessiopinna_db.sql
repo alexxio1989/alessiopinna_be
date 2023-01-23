f6552bd7c52f3dfbeb8775dbb312ff71068473f4
alexxio1989 <alessiopinna.elis@gmail.com>
2023-01-23 19:29:39 +0100

    avanzamento

--- src/main/resources/alessiopinna_db.sql
+++ src/main/resources/alessiopinna_db.sql
@@ -163,3 +163,6 @@ CREATE TABLE IF NOT EXISTS `freedb_alessiopinna`.`acquisto` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
+
+INSERT INTO tpl_utente (codice, descrizione) VALUES ('U', 'USER');
+INSERT INTO tpl_utente (codice, descrizione) VALUES ('SU', 'SUPER_USER');
